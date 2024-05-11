d3574b86b581f63ba686bfa2c58d5d3eb06dad25
Vadym Tarasov <vadym.tarasov@gmail.com>
2024-05-11 14:53:27 +0300

    Modify script

--- scripts/containerapp/deploy_backend_and_frontend.sh
+++ scripts/containerapp/deploy_backend_and_frontend.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 SUFFIX=$RANDOM
-RESOURCE_GROUP="album-containerapps-"$SUFFIX
+RESOURCE_GROUP="rg-album-containerapps-"$SUFFIX
 LOCATION="eastus"
 ENVIRONMENT="env-album-containerapps-vt-"$SUFFIX
 API_NAME="album-api-"$SUFFIX
 FRONTEND_NAME="album-ui-"$SUFFIX
 GITHUB_USERNAME="vive-there"
-
+ALBUM_API_SRC_PATH="$HOME/azure/repos/code-to-cloud-album-api"
+ALBUM_UI_SRC_PATH="$HOME/azure/repos/code-to-cloud-album-ui"
 ACR_NAME="acaalbumsvt"$SUFFIX
+EXPOSE_PORT=3502
+#Login
+#az login --use-device-code
 
 echo "----> Create resource group $RESOURCE_GROUP"
-az group create --name $RESOURCE_GROUP --location "$LOCATION"
+az group create \
+--name $RESOURCE_GROUP \
+--location "$LOCATION"
 
 echo "----> Create an Azure Container Registry $ACR_NAME"
-az acr create --resource-group $RESOURCE_GROUP --name $ACR_NAME --sku Basic --admin-enabled true
+az acr create \
+--resource-group $RESOURCE_GROUP \
+--name $ACR_NAME \
+--sku Basic \
+--admin-enabled true
 
 echo "----> Build the container with ACR and push image into ACR repo"
-az acr build --registry $ACR_NAME --resource-group $RESOURCE_GROUP --image $API_NAME -f ./code-to-cloud/src/Dockerfile ./code-to-cloud/src/.
+az acr build \
+--registry $ACR_NAME \
+--resource-group $RESOURCE_GROUP \
+--image $API_NAME \
+--file $ALBUM_API_SRC_PATH/src/Dockerfile \
+--build-arg EXPOSE_PORT=$EXPOSE_PORT \
+$ALBUM_API_SRC_PATH/src/.
 
 echo "----> Create a Container Apps environment $ENVIRONMENT"
 az containerapp env create \
@@ -25,13 +41,12 @@ az containerapp env create \
 --location $LOCATION
 
 echo "----> Deploy your image $ACR_NAME.azurecr.io/$API_NAME to a container app $API_NAME"
-#Check target port in Dockerfile!!! Use target port from EXPOSE in Dockerfile
 az containerapp create \
 --name $API_NAME \
 --resource-group $RESOURCE_GROUP \
 --environment $ENVIRONMENT \
 --image $ACR_NAME.azurecr.io/$API_NAME \
---target-port 8080 \
+--target-port $EXPOSE_PORT \
 --ingress "external" \
 --registry-server $ACR_NAME.azurecr.io \
 --query properties.configuration.ingress.fqdn
@@ -45,8 +60,7 @@ az acr build \
 --registry $ACR_NAME \
 --resource-group $RESOURCE_GROUP \
 --image $FRONTEND_NAME \
---file ./code-to-cloud-ui/src/Dockerfile \
-./code-to-cloud-ui/src/.
+--file $ALBUM_UI_SRC_PATH/src/Dockerfile $ALBUM_UI_SRC_PATH/src/.
 
 echo "----> Deploy front end application $FRONTEND_NAME image $ACR_NAME.azurecr.io/$FRONTEND_NAME"
 az containerapp create \
