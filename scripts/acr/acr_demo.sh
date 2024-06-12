b524ba7b6178d17f29469d2e591449d83aa0db79
Vadym Tarasov <vadym.tarasov@gmail.com>
2024-06-12 18:20:55 +0300

    Modify the scripts

--- scripts/acr/acr_demo.sh
+++ scripts/acr/acr_demo.sh
@@ -5,17 +5,25 @@ LOCATION="eastus"
 ACR_REGISTRY_NAME="vivethereacr"$RANDOM_SUFFIX
 
 az group create \
---name $RESOURCE_GROUP \
+--name ${RESOURCE_GROUP} \
 --location $LOCATION
 
 az acr create \
---name $ACR_REGISTRY_NAME \
---resource-group $RESOURCE_GROUP \
---sku Basic
+--name ${ACR_REGISTRY_NAME} \
+--resource-group ${RESOURCE_GROUP} \
+--sku Premium \
+--zone-redundancy enabled
+
+#Create replica
+az acr replication create \
+--registry ${ACR_REGISTRY_NAME} \
+--resource-group ${RESOURCE_GROUP} \
+--location southcentralus \
+--zone-redundancy enabled
 
 # login to acr
 az acr login \
---name $ACR_REGISTRY_NAME
+--name ${ACR_REGISTRY_NAME}
 
 # The following example pulls the hello-world image from a public Microsoft Container Registry to your local computer
 # or use your own local image
@@ -24,33 +32,33 @@ docker pull mcr.microsoft.com/hello-world
 # Before you can push an image to your registry, you must tag it with the fully qualified name of your registry login server. 
 # The login server name is in the format <registry-name>.azurecr.io (must be all lowercase), 
 # for example, mycontainerregistryapl2003.azurecr.io.
-docker tag mcr.microsoft.com/hello-world $ACR_REGISTRY_NAME.azurecr.io/hello-world:1.0
+docker tag mcr.microsoft.com/hello-world ${ACR_REGISTRY_NAME}.azurecr.io/hello-world:1.0
 
 # push image to ACR
-docker push $ACR_REGISTRY_NAME.azurecr.io/hello-world:1.0
+docker push ${ACR_REGISTRY_NAME}.azurecr.io/hello-world:1.0
 
-az acr repository list --name $ACR_REGISTRY_NAME
+az acr repository list --name ${ACR_REGISTRY_NAME}
 
 # delete local image if needed
-docker rmi $ACR_REGISTRY_NAME.azurecr.io/hello-world:1.0
+docker rmi ${ACR_REGISTRY_NAME}.azurecr.io/hello-world:1.0
 
 # pull from ACR to local
-docker pull $ACR_REGISTRY_NAME.azurecr.io/hello-world:1.0
+docker pull ${ACR_REGISTRY_NAME}.azurecr.io/hello-world:1.0
 
 # run
-docker run $ACR_REGISTRY_NAME.azurecr.io/hello-world:1.0
+docker run ${ACR_REGISTRY_NAME}.azurecr.io/hello-world:1.0
 
 echo "Show usage ${ACR_REGISTRY_NAME}"
-az acr show-usage --name $ACR_REGISTRY_NAME -g $RESOURCE_GROUP
+az acr show-usage --name ${ACR_REGISTRY_NAME} -g ${RESOURCE_GROUP}
 
-echo "Press any key to delete resource group $RESOURCE_GROUP ..."
-read -s -n 1
+echo "Press any key to delete resource group ${RESOURCE_GROUP} ..."
+read -n 1
 
 echo "Sleep"
 sleep 1
 
 echo "Delete resource group ${RESOURCE_GROUP}"
-az group delete --name $RESOURCE_GROUP --no-wait --yes
+az group delete --name ${RESOURCE_GROUP} --no-wait --yes
 az group list -o table
 
 
--- scripts/acr/acr_simple.sh
+++ scripts/acr/acr_simple.sh
@@ -18,8 +18,49 @@ az acr login --name ${ACR_REGISTRY_NAME}
 docker pull alpine:latest
 docker tag alpine:latest ${ACR_REGISTRY_NAME}.azurecr.io/alpinetest:0.1
 docker push ${ACR_REGISTRY_NAME}.azurecr.io/alpinetest:0.1
+
+docker tag alpine:latest ${ACR_REGISTRY_NAME}.azurecr.io/alpinetest:0.2
+docker push ${ACR_REGISTRY_NAME}.azurecr.io/alpinetest:0.2
+
+docker tag alpine:latest ${ACR_REGISTRY_NAME}.azurecr.io/alpinetest:0.3
+docker push ${ACR_REGISTRY_NAME}.azurecr.io/alpinetest:0.3
+
 docker rmi alpine:latest
 docker rmi ${ACR_REGISTRY_NAME}.azurecr.io/alpinetest:0.1
-docker image ls
-docker pull ${ACR_REGISTRY_NAME}.azurecr.io/alpinetest:0.1
-docker run -i ${ACR_REGISTRY_NAME}.azurecr.io/alpinetest:0.1
+#docker image ls
+#docker pull ${ACR_REGISTRY_NAME}.azurecr.io/alpinetest:0.1
+#docker run -i ${ACR_REGISTRY_NAME}.azurecr.io/alpinetest:0.1
+
+#Show the current repository attributes
+az acr repository show \
+--name ${ACR_REGISTRY_NAME} \
+--repository alpinetest
+
+#Show the image attributes
+DIGEST=$(echo $(az acr repository show \
+--name ${ACR_REGISTRY_NAME} \
+--image alpinetest:0.1 \
+--query "digest" \
+--output tsv) | tr -d "\t\r\n ")
+echo $DIGEST
+
+az acr manifest list --registry ${ACR_REGISTRY_NAME} --name alpinetest
+az acr manifest show --registry ${ACR_REGISTRY_NAME} --name alpinetest:0.2
+az acr manifest show-metadata --registry ${ACR_REGISTRY_NAME} --name alpinetest:0.2
+
+#Prevent deleting
+az acr manifest update-metadata --registry ${ACR_REGISTRY_NAME} --name alpinetest:0.2 --delete-enabled false
+
+az acr repository delete \
+--name ${ACR_REGISTRY_NAME} \
+--image alpinetest:0.2
+#--image alpinetest@${DIGEST}
+
+#Allow deleting
+az acr manifest update-metadata --registry ${ACR_REGISTRY_NAME} --name alpinetest:0.2 --delete-enabled true
+
+az acr repository delete \
+--name ${ACR_REGISTRY_NAME} \
+--image alpinetest:0.2
+
+az group delete --name ${RESOURCE_GROUP} --no-wait
