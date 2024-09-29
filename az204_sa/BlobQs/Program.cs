51d8e5cf4435d6a8320d3f62a61d47d85698146a
Vadym Tarasov <vadym.tarasov@gmail.com>
2024-09-29 16:16:36 +0300

    Add quickstart project

---
+++ az204_sa/BlobQs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+const string BLOB_CLINET_ENDPOINT = "https://vivethere18944.blob.core.windows.net/";
+
+var blobServiceClient = new BlobServiceClient(
+    new Uri(BLOB_CLINET_ENDPOINT),
+    new DefaultAzureCredential()
+);
+
+
+
