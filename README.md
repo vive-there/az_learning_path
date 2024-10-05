0291ed91b203897331410f5902a6e8701755670b
Vadym Tarasov <vadym.tarasov@gmail.com>
2024-10-05 16:40:13 +0300

    - Add encrypt and decrypt client demo

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # My Azure Learning Path
+``export AZURE_TENANT_ID=${echo $(az account show --query tenantId -o tsv) | tr -d '\n\t\r'}``
 
 ## References
 1. [HackTricks](https://cloud.hacktricks.xyz/pentesting-cloud/azure-security/az-azuread)
 2. https://github.com/Azure/azure-sdk-for-net/tree/main/sdk/resourcemanager/Azure.ResourceManager/samples
 3. https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md
 4. https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/containerinstance/Azure.ResourceManager.ContainerInstance/samples/Generated/Samples/Sample_ContainerGroupResource.cs
+5. [Steps to resolve “DefaultAzureCredential failed to retrieve a token” error](https://dotnetdevlife.wordpress.com/2022/02/07/steps-to-resolve-defaultazurecredential-failed-to-retrieve-a-token-error/)
--- az204_sa/BlobEncryptionKeyVault/Program.cs
+++ az204_sa/BlobEncryptionKeyVault/Program.cs
@@ -8,5 +8,74 @@ using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Azure.Storage.Blobs.Specialized;
 
-var keyName="sakey"
-var keyVaultName=Environment
+using System.Net;
+using System.Text;
+
+var keyName = "sakey1";
+var keyVaultName = Environment.GetEnvironmentVariable("KEY_VAULT_NAME");
+var keyVaultUrl = $"https://{keyVaultName}.vault.azure.net";
+
+TokenCredential tokenCredential = new DefaultAzureCredential();
+
+var keyClient = new KeyClient(new Uri(keyVaultUrl), tokenCredential);
+
+Response<KeyVaultKey> key;
+
+try
+{
+    key = await keyClient.GetKeyAsync(keyName);
+}
+catch(RequestFailedException rfe) when (rfe.Status == (int)HttpStatusCode.NotFound)
+{
+    key = await keyClient.CreateKeyAsync(keyName, KeyType.Rsa);
+}
+catch
+{
+    // log
+    throw;
+}
+
+// Cryptography client and key resolver instances using Azure Key Vault client library
+CryptographyClient cryptoClient = keyClient.GetCryptographyClient(key.Value.Name, key.Value.Properties.Version);
+KeyResolver keyResolver = new KeyResolver(tokenCredential);
+
+//For existing key in keyvault
+//var keyVaultKeyUri = $"https://{keyVaultName}.vault.azure.net/keys/{keyName}";
+//CryptographyClient cryptoClient = new CryptographyClient(new Uri(keyVaultKeyUri), tokenCredential);
+
+
+ClientSideEncryptionOptions clientSideEncryptionOptions = new ClientSideEncryptionOptions(ClientSideEncryptionVersion.V2_0) 
+{ 
+    KeyEncryptionKey = cryptoClient,
+    KeyResolver = keyResolver,
+    // String value that the client library will use when calling IKeyEncryptionKey.WrapKey()
+    KeyWrapAlgorithm = "RSA-OAEP"
+};
+
+var blobClientOptions = new SpecializedBlobClientOptions 
+{
+    ClientSideEncryption = clientSideEncryptionOptions,
+};
+
+var accountName = Environment.GetEnvironmentVariable("SA_NAME");
+var blobServiceClient = new BlobServiceClient(new Uri($"https://{accountName}.blob.core.windows.net"), tokenCredential, blobClientOptions);
+var blobContainerClient = blobServiceClient.GetBlobContainerClient("test");
+var blobClient = blobContainerClient.GetBlobClient($"{Guid.NewGuid().ToString("n")}");
+
+var bytes = Encoding.UTF8.GetBytes("Bla bla bla KeyEncryptionKey = cryptoClient");
+using var stream = new MemoryStream(bytes);
+await blobClient.UploadAsync(stream);
+
+Console.WriteLine("Uploaded");
+Console.WriteLine("Downloading ...");
+
+var downloadedResult = await blobClient.DownloadStreamingAsync();
+
+Console.WriteLine(await BinaryData.FromStreamAsync(downloadedResult.Value.Content));
+
+
+
+
+
+
+
